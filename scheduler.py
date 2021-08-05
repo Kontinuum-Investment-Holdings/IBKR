cddb30b9134e761cc48ec417452c0d2b16b62fe0
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-05 15:52:48 +1200

    Refactored code.

--- scheduler.py
+++ scheduler.py
@@ -7,11 +7,11 @@ import constants
 from jobs import monitor_vix
 
 if __name__ == "__main__":
+    communication.telegram.send_message(constants.TELEGRAM_BOT_USERNAME, "IBKR Scheduler started", False)
     time.sleep(60)
 
     schedule.every(10).seconds.do(monitor_vix.do)
 
     while True:
-        communication.telegram.send_message(constants.TELEGRAM_BOT_USERNAME, "IBKR Scheduler started", False)
         schedule.run_pending()
         time.sleep(1)
