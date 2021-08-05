98bc34fd50dbee9d67cb3a793201a029cbd7e701
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-05 18:10:32 +1200

    Testing scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,13 +4,11 @@ import communication.telegram
 import schedule
 
 import constants
-from jobs import update_code_base
+from jobs import monitor_vix
 
 if __name__ == "__main__":
     communication.telegram.send_message(constants.TELEGRAM_BOT_USERNAME, "IBKR Scheduler started", False)
-    # time.sleep(60)
-
-    schedule.every(60).seconds.do(update_code_base.do)
+    schedule.every().day.at("18:15").do(monitor_vix.do)
 
     while True:
         schedule.run_pending()
