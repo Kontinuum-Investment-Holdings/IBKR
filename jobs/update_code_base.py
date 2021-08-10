734c1e273f0f99e32e1001aefa98276d7cb453e2
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-11 03:37:44 +1200

    Fixed bug where IBKR doesn't restart after codebase update.

--- jobs/update_code_base.py
+++ jobs/update_code_base.py
@@ -10,10 +10,17 @@ def pull_codebase(working_directory: str) -> None:
     os.system("git pull origin main")
 
 
+def restart_IBKR() -> None:
+    os.chdir(constants.PROJECT_DIRECTORY + "IBKR")
+    os.system("nohup python3 scheduler.py -m >> logs/ibkr.log 2>&1 &")
+    quit()
+
+
 @common.job("Update code base")
 def do() -> None:
-    pull_codebase(constants.PROJECT_DIRECTORY + "IBKR")
     pull_codebase(constants.PROJECT_DIRECTORY + "KIH_API")
+    pull_codebase(constants.PROJECT_DIRECTORY + "IBKR")
+    restart_IBKR()
 
 
 if __name__ == "__main__":
