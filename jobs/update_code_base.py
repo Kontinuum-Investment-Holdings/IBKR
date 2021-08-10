9913558c3d894ba4553ed93a97916309632209ae
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-11 03:19:05 +1200

    Changed name of branch from master to main in update_code_base.py

--- jobs/update_code_base.py
+++ jobs/update_code_base.py
@@ -6,14 +6,14 @@ import constants
 
 def pull_codebase(working_directory: str) -> None:
     os.chdir(working_directory)
-    os.system("git reset --hard origin/master")
-    os.system("git pull origin master")
+    os.system("git reset --hard origin/main")
+    os.system("git pull origin main")
 
 
 @common.job("Update code base")
 def do() -> None:
     pull_codebase(constants.PROJECT_DIRECTORY + "IBKR")
-    pull_codebase(constants.PROJECT_DIRECTORY + "ibeam")
+    pull_codebase(constants.PROJECT_DIRECTORY + "KIH_API")
 
 
 if __name__ == "__main__":
