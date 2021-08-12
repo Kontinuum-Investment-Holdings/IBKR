b6bee83171ee7b063fc654f74acba731abc9a426
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-12 23:36:02 +1200

    Added functionality to update and restart ibeam in update_code_base.py

--- jobs/update_code_base.py
+++ jobs/update_code_base.py
@@ -6,14 +6,14 @@ import constants
 
 @common.job("Update code base")
 def do() -> None:
-    global_common.update_code_base(constants.PROJECT_DIRECTORY + "KIH_API")
-    global_common.update_code_base(constants.PROJECT_DIRECTORY + "IBKR")
+    global_common.update_code_base(constants.PROJECT_DIRECTORY + "ibeam", "master")
+    common.restart_ibeam()
 
-    if __name__ == "__main__":
-        common.kill_all_IBKR_scheduler()
-
+    global_common.update_code_base(constants.PROJECT_DIRECTORY + "KIH_API", "main")
+    global_common.update_code_base(constants.PROJECT_DIRECTORY + "IBKR", "main")
     common.restart_IBKR()
 
 
 if __name__ == "__main__":
+    common.kill_all_IBKR_scheduler()
     do()
