a866169182c94fc79ab5ee5353a5c1687ec7b41e
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-12 23:27:24 +1200

    Satisfied mypy.

--- common.py
+++ common.py
@@ -54,7 +54,7 @@ def kill_all_IBKR_scheduler() -> None:
     global_common.kill_process("scheduler.py", "python3")
 
 
-def restart_ibeam():
+def restart_ibeam() -> None:
     os.chdir(constants.PROJECT_DIRECTORY + "ibeam")
     global_common.kill_process("ibeam_starter.py", "python3")
     global_common.run_command(["python3 ibeam/ibeam_starter.py >> logs/ibeam.log 2>&1", "nohup python3 ibeam/ibeam_starter.py -m >> logs/ibeam.log 2>&1 &"])
