5ed7f8c916314aee9aaacb187b1ead0f7735ba70
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-12 22:59:30 +1200

    Added functionality that will kill all schedulers when commands.py is to restart.

--- commands.py
+++ commands.py
@@ -1,11 +1,10 @@
 import sys
 
-import global_common
-
 import common
 
 if __name__ == "__main__":
     if sys.argv[1] == "kill":
         common.kill_all_IBKR_scheduler()
-    elif sys.argv[1] == "start":
+    elif sys.argv[1] == "restart":
+        common.kill_all_IBKR_scheduler()
         common.restart_IBKR()
