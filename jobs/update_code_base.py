c5657c1e0ad0b5a3811dcffc93f60865de71fa1a
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-09-04 22:45:50 +1200

    Removed the code that starts the scheduler after updating the code base.

--- jobs/update_code_base.py
+++ jobs/update_code_base.py
@@ -9,7 +9,6 @@ import constants
 def do() -> None:
     global_common.update_code_base(constants.PROJECT_DIRECTORY + "KIH_API", "main")
     global_common.update_code_base(constants.PROJECT_DIRECTORY + "IBKR", "main")
-    common.restart_IBKR()
 
 
 if __name__ == "__main__":
