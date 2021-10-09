22a2b77ecf3d8c3c1ed405d229d2ac24ee77dc28
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-10-09 21:03:10 +1300

    Changed check connection status to run only 5 times max.

--- jobs/check_connection_status.py
+++ jobs/check_connection_status.py
@@ -10,14 +10,16 @@ import constants
 
 @global_common.threaded
 @ibkr.common.job("Check connection status")
-def check_connection_status() -> None:
+def check_connection_status(number_of_tries: int = 0) -> None:
     is_connection_successful: bool = IBKR.is_connection_successful()
     if is_connection_successful:
         communication.telegram.send_message(constants.TELEGRAM_BOT_DEV_USERNAME, f"<b>Connection status: </b> <i>Successful</i>", True)
     else:
         communication.telegram.send_message(constants.TELEGRAM_BOT_USERNAME, f"<b><u>Connection status: </u></b> <i>Unsuccessful</i>", True)
         time.sleep(60)
-        check_connection_status()
+
+        if number_of_tries < 5:
+            check_connection_status(number_of_tries + 1)
 
 
 def do() -> None:
